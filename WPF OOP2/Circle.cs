445180da9a7b04434a6979c8343f07ca79f44849
MirceaSharp <mircea_vasile_petrescu@yahoo.com>
2021-01-24 17:31:21 +0100

    Initial commit

---
+++ WPF OOP2/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_OOP2
+{
+    class Circle
+    {
+        private double _radius;
+
+        public double Straal {get;set;}
+
+        public Circle(double Radius)
+        {
+            _radius = Radius;
+        }
+
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * _radius;
+        }
+        public double CalculateSurface()
+        {
+            return  Math.PI * Math.Pow(_radius, 2);
+        }
+        public string FormattedCircumference()
+        {
+            return CalculateCircumference().ToString("F");
+        }
+        public string FormattedSurface()
+        {
+            return CalculateSurface().ToString("F");
+        }
+
+    }
+}
